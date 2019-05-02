c8d8bd67a9f5982a9ee253c9c5a3ca1cd11b6694
Joao Neto <jpsneto1@gmail.com>
2019-05-02 14:46:11 -0300

    refactoring directory structure

---
+++ backend/app/models/restaurant.rb
@@ -0,0 +1,26 @@
+class Restaurant < ApplicationRecord
+  
+  has_many :product_categories
+  has_many :orders
+  has_many :reviews
+
+  belongs_to :category
+
+  validates_associated :category
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :delivery_tax, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+
+  enum status: { closed: 0, open: 1 }
+
+  has_one_attached :image
+  geocoded_by :address
+  after_validation :geocode
+  
+  def address
+    [street, number, city, state, "brazil"].compact.join(', ')
+  end
+
+end
